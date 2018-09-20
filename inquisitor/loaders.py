a2eef3d9a194a269efbbae0a5e75c0a4e845ad37
Brad Searle <bradleysearle@gmail.com>
2018-09-20 19:21:23 +1000

    moving things around

--- inquisitor/loaders.py
+++ inquisitor/loaders.py
@@ -7,11 +7,17 @@ from .constants import TEXTFSM_TEMPLATES_DIR
 
 
 def load_template(template, location=TEXTFSM_TEMPLATES_DIR):
-    with open(f'{location}/{template}', 'r') as f:
-        return textfsm.TextFSM(f)
+    try:
+        with open(f'{location}/{template}', 'r') as f:
+            return textfsm.TextFSM(f)
+    except FileNotFoundError:
+        raise FileNotFoundError(f'File not found: {location}/{template}')
 
 
 def load_config(location='~/.config'):
-    with open(os.path.expanduser(location), 'r') as f:
-        config = json.load(f)
+    try:
+        with open(os.path.expanduser(location), 'r') as f:
+            config = json.load(f)
+    except FileNotFoundError:
+        config = {}
     return config
